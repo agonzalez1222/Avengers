9797e9d7c431a166bdedfab2fc3416508a050980
Armando Gonzalez <agonzalez9@ggc.edu>
2017-02-20 16:45:10 -0500

    Add files via upload

---
+++ Dummy.java
@@ -0,0 +1,18 @@
+
+public class Dummy 
+{
+
+	public static void main(String[] args) 
+	{
+		String team = "Hello Team";
+		for(int i = 0; i < team.length(); i++)
+		{
+			int n = 1;
+			n++;
+			System.out.println(n);
+			
+		}
+		System.out.println(team);
+	}
+
+}
