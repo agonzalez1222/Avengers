42e80c0af0977a640a243af1b8ad60d442339a8d
John P <John P>
2017-02-20 17:10:02 -0500

    test2

--- src/Dummy.java
+++ src/Dummy.java
@@ -4,14 +4,15 @@ public class Dummy
 
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
+
 		System.out.println("Hello, World!");
-=======
+
 
 		System.out.println ("whats good?");
 		
 		//yo
->>>>>>> 434d61b4eedc85310b09985cc10e098c68b02445
+		//test 2
+
 	}
 
 }
