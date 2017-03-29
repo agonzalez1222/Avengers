543788228e69673163c788b40b5ae332c80efb22
Magdalena Ledezma <Magdalena Ledezma>
2017-03-29 16:42:41 -0400

    uploading game class

---
+++ src/Game.java
@@ -0,0 +1,42 @@
+
+public class Game {
+
+	private int puzzlescompleted;
+	private int currentRoom;
+	private int currentScore;
+	
+	
+	public static void exitGame(){
+		
+	}
+	
+	public static void loadGame(){
+		
+	}
+	public static void openHelp(){
+		
+	}
+	
+	public static void viewInventory(){
+		
+	}
+	
+	public static void viewMap(){
+		
+	}
+	
+	public static void viewScore(){
+		
+	}
+	
+	public static void saveGame(){
+		
+	}
+	
+	}
+	
+	
+	
+	
+
+
