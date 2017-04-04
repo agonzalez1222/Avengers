3f27de4801fe7254b43ee2f10d462f64723f091a
Armaneo <agonzalez9@ggc.edu>
2017-04-04 12:51:49 -0400

    Updated Monster Class

--- src/Monster.java
+++ src/Monster.java
@@ -2,7 +2,19 @@
 public class Monster 
 {
 	private int health;
-	private int damageDealt;
+	private  int damageDealt;
 	private int damageTaken;
+	
+	protected int dealDamage()
+	{
+		
+		this.damageDealt = (int)(Math.random() * 5 + 1);
+		return this.damageDealt;
+	}
 	 
+	protected void takeDamage()
+	{
+		
+	}
+	
 }
---
+++ src/MonsterTester.java
@@ -0,0 +1,11 @@
+
+public class MonsterTester 
+{
+
+	public static void main(String[] args) 
+	{
+		Monster insaneCrewPerson = new Monster();
+		System.out.println(insaneCrewPerson.dealDamage());;
+	}
+
+}
