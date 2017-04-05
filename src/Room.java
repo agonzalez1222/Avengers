96ba7999d6020eb13f1d968188d8d15fe5560c4a
apancha <apanchatcharam@ggc.edu>
2017-04-04 22:37:20 -0400

    test

--- src/Room.java
+++ src/Room.java
@@ -1,4 +1,20 @@
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Room {
+	
+	//ArrayList<Integer> doorNumber = new ArrayList<Integer>();
 
+//	int[] doors = new int[40];{
+//	
+//	doors[0] = 1;
+//	doors[1] = 1;
+//	doors[2] = 1;
+//	doors[3] = 1;
+//	
+//	for (int i=0; i<doors.length; i++)
+//	{
+//	  System.out.println(doors[i]);
+//	}
+//
 }
